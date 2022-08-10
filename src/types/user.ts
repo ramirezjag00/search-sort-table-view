a0ce37207ee9a63e0ad66c046a86ce21a6c6cf5b
Andrey Ramirez <ramirez.jag00@gmail.com>
2022-08-11 02:27:15 +0800

    Add User type

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+type User = {
+  readonly name: string
+  readonly age: number
+  // TODO: ADDITIONAL TEST TYPES
+  // readonly foo: string
+  // readonly bar: string
+  // readonly fizz: string
+  // readonly buzz: string
+}
+
+export default User
