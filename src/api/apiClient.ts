3d7d29f019d96f38b0d91cdbe2eef58474836f5b
Andrey Ramirez <ramirez.jag00@gmail.com>
2022-08-12 00:44:47 +0800

    Add test data

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -6,30 +6,43 @@ class ApiClient {
       name: 'Matthew',
       age: 20,
       // TODO: ADDITIONAL TEST DATA
-      // foo: 'Foo',
-      // bar: 'Bar',
-      // fizz: 'Fizz',
-      // buzz: 'Buzz',
+      // foo: Math.random().toString(36).slice(2, 7),
+      // bar: Math.random().toString(36).slice(2, 7),
+      // fizz: Math.random().toString(36).slice(2, 7),
+      // buzz: Math.random().toString(36).slice(2, 7),
     },
     {
       name: 'Alexander',
       age: 25,
       // TODO: ADDITIONAL TEST DATA
-      // foo: 'Foo',
-      // bar: 'Bar',
-      // fizz: 'Fizz',
-      // buzz: 'Buzz',
+      // foo: Math.random().toString(36).slice(2, 7),
+      // bar: Math.random().toString(36).slice(2, 7),
+      // fizz: Math.random().toString(36).slice(2, 7),
+      // buzz: Math.random().toString(36).slice(2, 7),
     },
     {
       name: 'Samuel',
       age: 22,
       // TODO: ADDITIONAL TEST DATA
-      // foo: 'Foo',
-      // bar: 'Bar',
-      // fizz: 'Fizz',
-      // buzz: 'Buzz',
+      // foo: Math.random().toString(36).slice(2, 7),
+      // bar: Math.random().toString(36).slice(2, 7),
+      // fizz: Math.random().toString(36).slice(2, 7),
+      // buzz: Math.random().toString(36).slice(2, 7),
     },
   ]
+  // TODO: ADDITIONAL TEST DATA
+  // .concat(
+  //   ...Array.from(Array(50).keys()).map(() => {
+  //     return {
+  //       name: Math.random().toString(36).slice(2, 7),
+  //       age: Math.floor(Math.random() * 100),
+  //       foo: Math.random().toString(36).slice(2, 7),
+  //       bar: Math.random().toString(36).slice(2, 7),
+  //       fizz: Math.random().toString(36).slice(2, 7),
+  //       buzz: Math.random().toString(36).slice(2, 7),
+  //     }
+  //   }),
+  // )
 
   public static async fetchUsers(): Promise<User[]> {
     return Promise.resolve(ApiClient.users)
---
+++ src/constants/table.ts
@@ -0,0 +1,13 @@
+import { RowData } from '@customtypes/row'
+
+const EMPTY_USER_TABLE_DATA: RowData = {
+  name: 'N/A',
+  age: 'N/A',
+  // TODO: ADDITIONAL TEST DATA
+  //   foo: 'N/A',
+  //   bar: 'N/A',
+  //   fizz: 'N/A',
+  //   buzz: 'N/A',
+}
+
+export default EMPTY_USER_TABLE_DATA
